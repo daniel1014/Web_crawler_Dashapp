58014ad6464bd38ddf9c8bdfa21e0c145aa65be9
daniel1014 <chuenlik@hotmail.com>
2024-01-11 16:15:20 +0000

    Refactor database connection and data upload functions

--- db_connection.py
+++ db_connection.py
@@ -1,14 +1,11 @@
 from dotenv import dotenv_values
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 import urllib
-import re
-from pathlib import Path
-import pickle
 import pandas as pd
-import os
+import pyodbc
 
 def read_db_conn_details() -> dict:
-    """Output of this should be passed to df_from_db "db_conn_details" arg"""
+    """Output of this should be passed to the create_engine function of sqlalchemy"""
     config = dotenv_values(".env")
     params = urllib.parse.quote_plus(
         "Driver={SQL Server};"
@@ -23,21 +20,43 @@ def df_from_db(query: str) -> pd.DataFrame:
     params = read_db_conn_details()
     conn_str = "mssql+pyodbc:///?odbc_connect=" + params
     engine = create_engine(conn_str)
-    df = pd.read_sql(query, engine)
-    return df
+    try:
+        df = pd.read_sql_query(text(query), con=engine.connect())
+        return df
+    except Exception as e:
+        print(f"Error reading data from the database: {str(e)}")
+        return None
 
-def store_results(result: list):
-    params = read_db_conn_details()
-    conn_str = f"mssql+pyodbc:///?odbc_connect={params}"
-    engine = create_engine(conn_str)
-    df = pd.DataFrame(result)
-    df.to_sql(name='news_output', con=engine, schema='BMT', if_exists='append', index=False)
+def upload_data(data: list, table_name: str, username: str):
+    db_conn_details = dotenv_values(".env")
 
-# db_conn_details = read_db_conn_details()
-# query = "SELECT aecom_code2 FROM BMT.Asset"
-# df = df_from_db(query, db_conn_details)
-# print(df)
+    # Establish a connection to the database
+    cnxn = pyodbc.connect(
+        f"DRIVER=ODBC Driver 17 for SQL Server;"
+        f"SERVER={db_conn_details['Server']};"
+        f"DATABASE={db_conn_details['Database']};"
+        f"UID={db_conn_details['User']};"
+        f"PWD={db_conn_details['Pass']}"
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = cnxn.cursor()
+
+    # Upload data to the table
+    schema = 'BMT'
+    for record in data:
+        insert_query = f'''
+            INSERT INTO {schema}.{table_name} (supplier, focus, num_search, username)
+            VALUES (?, ?, ?, ?)
+        '''
+        cursor.execute(insert_query, record['supplier'], record['focus'], record['num_search'], username)
+
+    # Commit the changes to the database
+    cnxn.commit()
     
-# def get_all_results(supplier_input, focus_input):
-#     if supplier_input != "" and focus_input != "":
-#         query = f"SELECT * FROM dbo.all_results WHERE supplier = '{supplier_input}' AND focus = '{focus_input}'"
+    # Close the cursor and connection
+    cursor.close()
+    cnxn.close()
+
+
+
--- main_dash.py
+++ main_dash.py
@@ -12,27 +12,35 @@ import gsearch as s
 # Initialize the Dash app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MORPH])
 data_demo = [{'supplier':"Enercon",'focus':'Supply Chain','num_search':'10'}]
-df_database_demo = pd.DataFrame(data_demo)
+df_database_demo = pd.DataFrame(data_demo).reset_index()
 new_line = [{'supplier':'','focus':'','num_search':'10'}]
-df_new_line = pd.DataFrame(new_line)
+# df_new_line = pd.DataFrame(new_line)
 
 # Define the app layout
 app.layout = dbc.Container([
-    html.H1("News Scraper"),
+    html.H1("News Scraper Tool (under development)"),
+    dcc.Markdown('''This tool is used to scrape news from Google Search Engine. The input is the supplier and focus. The output is the news title, date, description and URL. The output can be downloaded as Excel or CSV file. The input can be uploaded to AECOM database.''', style={'font-size': '18px'}),
     dcc.Download(id="download"),
+    dcc.Store(id='selected-rows-store', data=[]),
     dbc.Row([
         dbc.Col([
-            dbc.Input(id={'type':'supplier-input', 'index': 1}, type='text', placeholder="Enter Supplier", className='mb-2'),
-        ],width=2),
+            dbc.Input(id='username', type='text', placeholder="Enter Your Username", className='mb-2')
+            ],width=3),
         dbc.Col([
-            dbc.Input(id={'type':'focus-input', 'index': 1}, type='text', placeholder="Enter Focus", className='mb-2'),
-        ], width=2),
-        ], id='input-container'),
+            dbc.Button('Login (load your data)', id='login-button', className='text-light bg-primary'),
+            html.Div(id='login-status')  # Placeholder for the alert callback
+            ])
+        ]),
     dbc.Row([
         dbc.Col([
             AgGrid(
                 id="table_input",
                 columnDefs=[
+                    {
+                        "headerName": "Index", 
+                        "field": "index",
+                        'width': 30,
+                        "checkboxSelection": True,},
                     {
                         "headerName": "Supplier",
                         "field": "supplier",
@@ -54,7 +62,7 @@ app.layout = dbc.Container([
                         "field": "num_search",
                         "cellEditor": "agSelectCellEditor",
                         "cellEditorParams": {
-                            "values": [str(i) for i in range(5, 31, 5)]
+                            "values": [str(i) for i in range(5, 11, 5)]
                         },
                     },
                 ],
@@ -62,25 +70,37 @@ app.layout = dbc.Container([
                 columnSize="sizeToFit",
                 defaultColDef={'editable': True, 'sortable': True, 'filter': True},
                 style={'height': '200px'},
+                dashGridOptions={"rowSelection": "multiple",},
             )
         ])
     ]),
     dbc.Row([
         dbc.Col([
-            dbc.Button('Add Input', id='add-input-button', n_clicks=0, className='mb-2'),
-            dbc.Button('Search', id='search-button', n_clicks=0, className='mb-2'),
-            dbc.Button('Upload to AECOM Database', id='upload-button', n_clicks=0, className='mb-2'),
+            dbc.Button('Add Input', id='add-input-button', className='bg-light'),
+            dbc.Button('Delete Selected Row', id='delete-input-button', className='bg-light'),
+            dbc.Button('Search', id='search-button', n_clicks=0, className='text-success-emphasis bg-light'),
+            dbc.Button('Upload to AECOM Database', id='upload-button', className='text-warning-emphasis bg-light'),
+            html.Div(id='upload-status')  # Placeholder for the alert callback
         ]),
     ]),
     dbc.Row(
         dbc.Col(
             DataTable(
-                id='table',
+                id='table_output',
                 columns=[{"name": i, "id": i} for i in ['Supplier', 'Focus', 'Title', 'Date', 'Description', 'URL']],
                 data=[],
                 page_action='custom',
                 page_current=0,
-                page_size=10
+                page_size=10,
+                style_cell={'textAlign': 'left'},
+                style_data_conditional=[
+                    {
+                        'if': {'column_id': 'URL'},
+                        'textDecoration': 'underline',
+                        'cursor': 'pointer'
+                    }
+                ],
+                tooltip_duration=None
             ), 
             id='output-container'
         )
@@ -97,33 +117,71 @@ app.layout = dbc.Container([
             ),width=4,
         ),
         dbc.Col(
-            dbc.Button('Download Result', id='save-button', n_clicks=0),
+            dbc.Button('Download Result', id='save-button', className='text-light bg-info'),
         width=2
         )
-    ]),
-    dcc.Store(id='all-results-store'),
+    ])
 ], fluid=True)
 
-# Callback to add input fields in AgGrid table
 @app.callback(
-    Output('table_input', 'rowData'),
+    Output('selected-rows-store', 'data'),
+    Input('table_input', 'selectedRows')
+)
+def update_selected_rows(selected):
+    # if selected is None:
+    #     raise dash.exceptions.PreventUpdate
+    selected_store = selected
+    print(selected_store)
+    return selected_store
+
+@app.callback(
+    [Output('table_input', 'rowData'), Output('login-status', 'children')],
+    [Input('login-button', 'n_clicks'),
     Input('add-input-button', 'n_clicks'),
-    Input('table_input', 'rowData'),
+    Input('delete-input-button', 'n_clicks')],
+    [State('username', 'value'),
+    State('table_input', 'rowData'),
+    State('selected-rows-store', 'data')],
     prevent_initial_call=True
 )
+def update_table(login_clicks, add_input_clicks, delete_clicks, username, all_rows, selected_rows):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        raise dash.exceptions.PreventUpdate
+    else:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
-def add_input(n_clicks, rows):
-    df_input = pd.DataFrame(rows)
-    if n_clicks > 0:
-        df_input = pd.concat([df_input,df_new_line], ignore_index=True)
-    return df_input.to_dict("records")
+    if button_id == 'login-button':
+        try:
+            df_database = db_connection.df_from_db(f"SELECT supplier, focus, num_search FROM BMT.NewsInput WHERE username = '{username}'")
+            df_database = df_database.reset_index(drop=True)
+            df_database['index'] = df_database.index
+            return df_database.to_dict("records"), dbc.Alert("Login Successful", duration=3000, class_name="alert-success")
+        except Exception as e:
+            return [], dbc.Alert(f"Login Failed. Error details:{e}", class_name="alert-warning")
+    elif button_id == 'add-input-button':
+        df_input = pd.DataFrame(all_rows)
+        df_input = df_input.reset_index(drop=True)
+        if df_input.empty:
+            df_new_input = pd.DataFrame(new_line, index=[0]).reset_index()
+        else:
+            df_new_input = pd.DataFrame(new_line, index=[df_input['index'].max() + 1]).reset_index()
+        df_input = pd.concat([df_input,df_new_input], ignore_index=True)
+        return df_input.to_dict("records"), dash.no_update
+    elif button_id == 'delete-input-button':
+        df_input = pd.DataFrame(all_rows)
+        selected_rows = [row['index'] for row in selected_rows]
+        df_input = df_input[~df_input['index'].isin(selected_rows)]
+        df_input.reset_index(drop=True,inplace=True)
+        df_input['index'] = df_input.index
+        return df_input.to_dict('records'), dash.no_update
 
 # Callback to update the page content
 @app.callback(
-    [Output('output-container', 'children'), Output('all-results-store', 'data')],
+    [Output('table_output', 'data'), Output('table_output', 'tooltip_data')],
     [Input('search-button', 'n_clicks'),
-     Input('table', 'page_current'),
-     Input('table', 'page_size')],
+     Input('table_output', 'page_current'),
+     Input('table_output', 'page_size')],
     [State('table_input', 'rowData')]
 )
 def update_output(n_clicks, page_current, page_size, table_input_data):
@@ -134,8 +192,9 @@ def update_output(n_clicks, page_current, page_size, table_input_data):
         for row in table_input_data:
             supplier_input = row['supplier']
             focus_input = row['focus']
+            num_search = int(row['num_search'])
             query = supplier_input + " " + focus_input 
-            search_results = s.google_search(query, s.my_api_key, s.my_cse_id, num=10)
+            search_results = s.google_search(query, s.my_api_key, s.my_cse_id, num=num_search)
             for result in search_results:
                 if " ... " in result['snippet']:
                     date, description = result['snippet'].split(" ... ", 1)
@@ -145,38 +204,21 @@ def update_output(n_clicks, page_current, page_size, table_input_data):
                 all_results.append({'Supplier': supplier_input, 'Focus': focus_input, 'Title': result['title'], 'Date' : date, 'Description': description, 'URL': result['link']})
         page_start = page_current * page_size
         page_end = page_start + page_size
-        
-        return DataTable(
-        id='table',
-        columns=[{"name": i, "id": i} for i in ['Supplier', 'Focus', 'Title', 'Date', 'Description', 'URL']],
-        data=all_results[page_start:page_end],
-        page_action='custom',
-        page_current=page_current,          # Not constant, it would change when clicking on next/previous page
-        page_size=page_size,                # constant value
-        style_cell={'textAlign': 'left'},
-        style_data_conditional=[
-            {
-                'if': {'column_id': 'URL'},
-                'textDecoration': 'underline',
-                'cursor': 'pointer'
-            }
-        ],
         tooltip_data=[
             {
                 column: {'value': str(value), 'type': 'markdown'}
                 for column, value in row.items()
             } for row in all_results
-        ],
-        tooltip_duration=None
-        ), all_results
+        ]
+        return all_results[page_start:page_end], tooltip_data
     else:
-        return DataTable(id='table'), []   # Default return value
+        return [], []   # Default return value
 
 # Callback to download the search results
 @app.callback(
     Output('download', 'data'),
     [Input('save-button', 'n_clicks')],
-    [State('all-results-store', 'data'),
+    [State('table_output', 'data'),
      State('dropdown', 'value')],
      prevent_initial_call=True
 )
@@ -188,18 +230,22 @@ def generate_excel(n_clicks, all_results, download_type):
     else:
         return dcc.send_data_frame(df.to_excel, "news_output.xlsx", sheet_name="news_output", index=False)
     
-# Callback to upload the search results to database
+# Callback to upload the search input to database
 @app.callback(
-    Output('upload-button', 'children'),
+    Output('upload-status', 'children'),
     [Input('upload-button', 'n_clicks')],
-    [State('all-results-store', 'data')]
+    [State('table_input', 'rowData'), State('username', 'value')],
+    prevent_initial_call=True
 )
-def upload_to_database(n_clicks, all_results):
+def upload_to_database_input(n_clicks, table_input_data, username):
     if n_clicks > 0:
-        db_connection.store_results(all_results)
-        return 'Upload Successful'
+        try:
+            db_connection.upload_data(table_input_data, 'NewsInput', username) 
+            return dbc.Alert("Upload Successful", duration=3000, class_name="alert-success")
+        except Exception as e:
+            return dbc.Alert("Upload Failed. Error details:{e}", class_name="alert-warning")
     else:
-        return 'Upload to AECOM Database'
+        return None
     
 if __name__ == '__main__':
     app.run_server(debug=True)
