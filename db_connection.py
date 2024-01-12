66fb1b0f68e4345c21143bfc3f7f5a1a630e4d6a
daniel1014 <chuenlik@hotmail.com>
2024-01-12 12:35:18 +0000

    Update database connection details

--- db_connection.py
+++ db_connection.py
@@ -3,18 +3,18 @@ from sqlalchemy import create_engine,text
 import urllib
 import pandas as pd
 import pyodbc
+import os
+
+config = dotenv_values(".env")
+server = os.getenv('Server')
+database = os.getenv('Database')
+user = os.getenv('User')
+password = os.getenv('Pass')
 
 def read_db_conn_details() -> dict:
     """Output of this should be passed to the create_engine function of sqlalchemy"""
-    config = dotenv_values(".env")
-    # params = urllib.parse.quote_plus(
-    #     "Driver={SQL Server};"
-    #     + f"Server=tcp:{config['Server']},1433;"
-    #     + f"Database={config['Database']};"
-    #     + f"Uid={config['User']};"
-    #     + f"Pwd={config['Pass']};"
-    # )
-    params = f"DRIVER=ODBC Driver 17 for SQL Server;SERVER=ratelibrary.database.windows.net;DATABASE=CostIntelIDA01;UID=RLsqladmin;PWD=4H$f8ci3nz;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30"
+
+    params = f"DRIVER=ODBC Driver 17 for SQL Server;SERVER={server};DATABASE={database};UID={user};PWD={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30"
     
     return params
 
@@ -26,15 +26,14 @@ def df_from_db(query: str) -> pd.DataFrame:
     return df
 
 def upload_data(data: list, table_name: str, username: str):
-    db_conn_details = dotenv_values(".env")
 
     # Establish a connection to the database
     cnxn = pyodbc.connect(
         f"DRIVER=ODBC Driver 17 for SQL Server;"
-        f"SERVER={db_conn_details['Server']};"
-        f"DATABASE={db_conn_details['Database']};"
-        f"UID={db_conn_details['User']};"
-        f"PWD={db_conn_details['Pass']};"
+        f"SERVER={config['Server']};"
+        f"DATABASE={config['Database']};"
+        f"UID={config['User']};"
+        f"PWD={config['Pass']};"
         f"Encrypt=yes;"
         f"TrustServerCertificate=no;"
         f"Connection Timeout=30"
